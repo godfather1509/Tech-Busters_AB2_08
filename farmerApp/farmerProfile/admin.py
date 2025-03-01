9872f06d39941391b16ccf947160f26852a794c6
Ayush <ayushg1500@gmail.com>
2025-03-01 21:30:18 +0530

    INITIAL COMMIT

--- farmerApp/farmerProfile/admin.py
+++ farmerApp/farmerProfile/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Product_listing
 
+admin.site.register(Product_listing)
 # Register your models here.
--- farmerApp/farmerProfile/models.py
+++ farmerApp/farmerProfile/models.py
@@ -1,1 +1,20 @@
 from django.db import models
+
+class Product_listing(models.Model):
+
+    WEIGHT_UNITS=[
+        ("Kg","Kilogram"),
+        ("Tons","Tons"),
+        ("Qntls","Quintals")
+    ]
+
+    product_name=models.CharField(max_length=100)
+    unit_price=models.IntegerField()
+    quantity=models.CharField(max_length=100)
+    unit_quantity=models.CharField(max_length=100,choices=WEIGHT_UNITS)
+    harvest_date=models.DateField()
+    image=models.ImageField(upload_to='farmerProfile/')
+    description=models.TextField(default="")
+
+    def __str__(self):
+        return self.product_name
